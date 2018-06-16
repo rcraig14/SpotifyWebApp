c62202ad38234777122421e17cb4eda9855b1ac3
Oasis14 <hkyryan14@gmail.com>
2018-06-16 15:51:15 -0600

    Removed appkey.json and added an example, updated the requestHandler to read app data

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,11 @@
+// Get app data from file
+var fs = require("fs");
+var appConfigFile = fs.readFileSync("appkey.json");
+var appKeys = JSON.parse(appConfigFile);
+
+//set route file for html files
 const htmlFiles = __dirname+'/public/html/';
+
 
 module.exports = {
   home: function(request,response){
