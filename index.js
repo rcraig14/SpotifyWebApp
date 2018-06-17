c00fa2dbd7dd657433090dd0fa21a3b4e47ca026
Oasis14 <hkyryan14@gmail.com>
2018-06-16 19:35:45 -0600

    Basic framework for responsive design. Also started authentication with spotify

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
 var express = require('express');
 var app = express();
 var requestHandler = require('./requestHandlers.js');
+var fs = require('fs');
+var https = require('https');
 /* set up static files routes */
 app.use(express.static('public'));
 
@@ -9,9 +10,13 @@ app.use(express.static('public'));
 
 //Set app routes and handlers
 app.get('/', requestHandler.home);
+app.get('/login', requestHandler.login);
+app.get('/callback', requestHandler.callback);
 
-
-//Launch listening server on port 8081
-app.listen(8080, function () {
+//Launch listening server on port 8080
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+  }, app).listen(8080, function () {
   console.log('app listening on port 8080!')
-})
+});
--- requestHandlers.js
+++ requestHandlers.js
@@ -3,15 +3,30 @@ var fs = require("fs");
 var appConfigFile = fs.readFileSync("appkey.json");
 var appKeys = JSON.parse(appConfigFile);
 
+
+
 //set route file for html files
 const htmlFiles = __dirname+'/public/html/';
 
 
 module.exports = {
   home: function(request,response){
-    console.log(__dirname);
-    //response.send("arrived");
     response.sendFile(htmlFiles + 'index.html');
+  },
+
+  login: function(request, response){
+    var scopes = 'user-read-private';
+    var redirect_uri = 'http://localhost:8080';
+    response.redirect('https://accounts.spotify.com/authorize'
+      +'?response_type=code&client_id='+appKeys.clientId
+      +(scopes ? '&scope=' + encodeURIComponent(scopes): '')
+      +'&redirect_uri='+encodeURIComponent(redirect_uri)
+      +'&show_dialog=false')
+  },
+
+  callback: function(request, response){
+    console.log("reached");
+    console.log(request);
   }
 
 
